80e228f997df74f9cd04f36c9306d7e29528365f
JasonMorta <jason.morta23@gmail.com>
2022-07-19 22:36:17 +0200

    update

--- pages/projects.js
+++ pages/projects.js
@@ -1,19 +1,19 @@
-import Image from 'next/image'
+
 import Layout from '../components/Layout'
 
 const projects = [
             {
-               "img": "/../static/images/cardGame.jpg",
+               "pics": "/../static/images/cardGame.jpg",
                "name": "React Card Game",
                "des":   "Built a React game and hosted in on Heroku"
             },
             {
-               "img": "/../static/images/iTunesMusicAPI.jpg",
+               "pics": "/../static/images/iTunesMusicAPI.jpg",
                "name": "iTunes Music API",
                "des":   "React App that  make suse of the itunes API, and allows a user to find media content in the apple store."
             },
             {
-               "img": "/../static/images/onlineStore.jpg",
+               "pics": "/../static/images/onlineStore.jpg",
                "name": "Online Store",
                "des":   "This store was make using HTM, CSS and JS. A user can add items to the cars, and make cart updates. Card data is saved in localStorage"
             },
@@ -21,7 +21,7 @@ const projects = [
 
          const list = projects.map(item =>(
             <div className="card">
-               <Image  src={item.img} alt="screenshot" width={300} height={300}/>
+               <img src={item.pics} alt="screenshot" width={300} height={300}/>
                <div className="container">
                   <h4><b>{item.name}</b></h4> 
                   <p>{item.des}</p> 
